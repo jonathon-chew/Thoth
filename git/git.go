cb3244314d1dbdeb6e9f8549e56f56d39dd5ffbd
Jonathon Chew <jonchew626@hotmail.com>
2026-01-04 21:40:08 +0000

    new: github specific functions move to their own file, as well as structus; refactor: gitlab/github structs to reflect their useage to stop collisions

--- git/git.go
+++ git/git.go
@@ -2,11 +2,8 @@ package git
 
 import (
 	"bytes"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"os/exec"
 	"slices"
@@ -16,58 +13,6 @@ import (
 	aphrodite "github.com/jonathon-chew/Aphrodite"
 	utils "github.com/jonathon-chew/Thoth/Utils"
 )
-
-// GITHUB STRUCTS
-type Assignee struct {
-	Login string `json:"login"`
-	Type  string `json:"type"`
-}
-
-type Issue struct {
-	Title     string   `json:"title"`
-	Body      string   `json:"body"`
-	Milestone int      `json:"milestone,omitempty"`
-	Label     []string `json:"labels,omitempty"`
-	Assignees string   `json:"assignees,omitempty"`
-}
-
-type Label struct {
-}
-
-type GithubIssueResponse struct {
-	Url            string `json:"url"`
-	Repository_url string `json:"repository_url"`
-	Labels_url     string `json:"labels_url"`
-	Comments_url   string `json:"comments_url"`
-	Events_url     string `json:"events_url"`
-	Id             int    `json:"id"`
-	Node_id        string `json:"node_id"`
-	Number         int    `json:"number"`
-	Title          string `json:"title"`
-	User           struct {
-		Login          string `json:"login"`
-		Id             int    `json:"id"`
-		Repos_url      string `json:"repos_url"`
-		Events_url     string `json:"events_url"`
-		Type           string `json:"type"`
-		User_view_type string `json:"user_view_type"`
-		Site_admin     bool   `json:"site_admin"`
-	} `json:"user"`
-	Labels             []Label    `json:"labels"`
-	State              string     `json:"state"`
-	State_Reason       string     `json:"state_reason"`
-	Locked             bool       `json:"locked"`
-	Assignee           Assignee   `json:"assignee"`
-	Assignees          []Assignee `json:"assignees"`
-	Comments           int        `json:"comments"`
-	Created_at         string     `json:"created_at"`
-	Updated_at         string     `json:"updated_at"`
-	Author_association string     `json:"author_association"`
-	Active_lock_reason string     `json:"active_lock_reason"`
-	Body               string     `json:"body"`
-	Message            string     `json:"message"`
-	Status             string     `json:"status"`
-}
 
 var HTTPStatusResponseMeanings = map[string]string{
 	"201": "Created",
@@ -294,7 +239,7 @@ func NewGitTag(argument string) error {
 		newTag = fmt.Sprintf("v%d.%d.%d", major, minor, newPatch)
 	}
 
-	cmd := exec.Command("git", "tag", newTag)
+	cmd := exec.Command("git", "tag", newTag, "-m", "Release Version: "+strings.Replace(newTag, "v", "", -1))
 
 	var out bytes.Buffer
 	var stderr bytes.Buffer
@@ -384,187 +329,4 @@ func GenericGitRequest() (Credentials, error) {
 	} else {
 		return credentials, fmt.Errorf("the remote origin is not github/gitlab, and the ability to create issues for %s is not currently implimented", remoteOrigin)
 	}
-}
-
-// LIST GIT ISSUES
-func ListGithubIssues(passedFromCLI bool) ([]GithubIssueResponse, error) {
-
-	var ResponseInstance []GithubIssueResponse
-
-	GitCredentials, err := GenericGitRequest()
-	if err != nil {
-		return ResponseInstance, err
-	}
-
-	request, err := http.NewRequest("GET", fmt.Sprintf("https://api.github.com/repos/%s/%s/issues?state=all", GitCredentials.Owner, GitCredentials.Repo), nil)
-	if err != nil {
-		return ResponseInstance, err
-	}
-
-	request.Header.Set("Accept", "application/vnd.github+json")
-	request.Header.Set("X-GitHub-Api-Version", "2022-11-28")
-	request.Header.Set("Authorization", fmt.Sprintf("token %s", GitCredentials.Token))
-
-	client := http.Client{}
-
-	req, err := client.Do(request)
-	if err != nil {
-		return ResponseInstance, err
-	}
-
-	defer req.Body.Close()
-
-	if !passedFromCLI {
-		fmt.Printf("The response was: %s, %s\n\n", req.Status, HTTPStatusResponseMeanings[req.Status])
-	}
-
-	responseBody, err := io.ReadAll(req.Body)
-	if err != nil {
-		return ResponseInstance, err
-	}
-
-	// fmt.Printf("Repsonse Body: %s\n\n", string(responseBody))
-
-	if err := json.Unmarshal(responseBody, &ResponseInstance); err != nil {
-		return ResponseInstance, fmt.Errorf("error unmarshalling response: %w", err)
-	}
-
-	if len(ResponseInstance) == 0 {
-		return ResponseInstance, errors.New("no GitHub issues found")
-	}
-
-	if req.StatusCode != http.StatusOK {
-		return ResponseInstance, fmt.Errorf("GitHub API error: %s", req.Status)
-	}
-
-	// fmt.Printf("ResponseInstance: %v\n\n", ResponseInstance)
-
-	// for _, response := range ResponseInstance {
-	// 	fmt.Println("The title for the response is: ", strings.TrimSpace(response.Title), " with ID: ", response.Id)
-	// }
-
-	return ResponseInstance, nil
-}
-
-func MakeGithubIssue(TITLE, BODY string) error {
-
-	// Get the credentials required
-	GithubCredentials, err := GenericGitRequest()
-	if err != nil {
-		return err
-	}
-
-	// Create the issue using a struct
-	issue := Issue{
-		Title: strings.TrimSpace(TITLE),
-		Body:  BODY,
-	}
-
-	// Convert the struct into JSON using the tags and Marshal
-	jsonData, err := json.Marshal(issue)
-	if err != nil {
-		return err
-	}
-
-	// Convert the JSON into bytes
-	requestBody := bytes.NewBuffer(jsonData)
-
-	// Make the request
-	request, err := http.NewRequest("POST", fmt.Sprintf("https://api.github.com/repos/%s/%s/issues", GithubCredentials.Owner, GithubCredentials.Repo), io.Reader(requestBody))
-	if err != nil {
-		fmt.Printf("Error making the HTTP request %s\n", err)
-		return err
-	}
-
-	// Set the required headers
-	request.Header.Set("Accept", "application/vnd.github+json")
-	request.Header.Set("X-GitHub-Api-Version", "2022-11-28")
-	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", GithubCredentials.Token))
-
-	// Make a new client
-	client := http.Client{}
-
-	// Complete the request - Client.Do because the http.NewRequest handles the method
-	req, err := client.Do(request)
-	if err != nil {
-		return err
-	}
-
-	if req.StatusCode != 200 && req.StatusCode != 201 {
-		fmt.Println(req.Body)
-		return fmt.Errorf("the response was not positive, %d", req.StatusCode)
-	}
-
-	fmt.Printf("The response was: %s, %s\n", req.Status, HTTPStatusResponseMeanings[req.Status])
-
-	return nil
-}
-
-// REMOVE GIT ISSUES
-// (#2) TODO: Add the ability to remove to dos which have been closed on github
-func RemoveLineDueToGithubIssue(line string, foundGithubIssues []GithubIssueResponse) (bool, error) {
-
-	// Loop through the issues and compare to the line
-	for _, issue := range foundGithubIssues {
-		if strings.Contains(strings.TrimSpace(line), issue.Title) {
-			err := CloseGithubIssue(&issue)
-			if err != nil {
-				return true, err // trying this out - as first half the of the function was "completed" successfully but the second half wasn't!
-			}
-			return true, nil
-		}
-	}
-
-	// If the loop didn't find anything return false and no error!
-	return false, nil
-}
-
-// (#3) TODO: Add the ability to close issues on github which have been removed from the code base
-
-func CloseGithubIssue(closeIssue *GithubIssueResponse) error {
-
-	// Put together the JSON message required to close an issue
-	closeIssue.State = "closed"
-	closeIssue.State_Reason = "completed"
-
-	// Get the credentials
-	GithubCredentials, err := GenericGitRequest()
-	if err != nil {
-		return err
-	}
-
-	// Convert the struct into JSON using the tags and Marshal
-	jsonData, err := json.Marshal(closeIssue)
-	if err != nil {
-		return err
-	}
-
-	// Convert the JSON into bytes
-	requestBody := bytes.NewBuffer(jsonData)
-
-	// Write the request
-	request, err := http.NewRequest("PATCH", fmt.Sprintf("https://api.github.com/repos/%s/%s/issues/%d", GithubCredentials.Owner, GithubCredentials.Repo, closeIssue.Number), requestBody)
-	if err != nil {
-		return err
-	}
-
-	// Set the required headers
-	request.Header.Set("Accept", "application/vnd.github+json")
-	request.Header.Set("X-GitHub-Api-Version", "2022-11-28")
-	request.Header.Set("Authorization", fmt.Sprintf("token %s", GithubCredentials.Token))
-
-	client := http.Client{}
-
-	// Make the request
-	closeGithubIssueResponse, clientErr := client.Do(request)
-	if clientErr != nil {
-		fmt.Printf("The response from github was: %s\n", HTTPStatusResponseMeanings[closeGithubIssueResponse.Status])
-		return clientErr
-	}
-
-	fmt.Printf("The response from github was: %s\n", HTTPStatusResponseMeanings[closeGithubIssueResponse.Status])
-
-	// Return if error?
-	return nil
-
 }
--- git/gitlab.go
+++ git/gitlab.go
@@ -30,7 +30,7 @@ type Create_Gitlab_Issue struct {
 	Epic_iid                                int      `json:"epic_iid"`
 }
 
-type Milestone struct {
+type Gitlab_Milestone struct {
 	Id          string `json:"id"`
 	Iid         string `json:"iid"`
 	Project_id  string `json:"project_id"`
@@ -46,51 +46,51 @@ type Milestone struct {
 	Web_url     string `json:"web_url"`
 }
 
-type Custom_attributes struct {
+type Gitlab_Custom_attributes struct {
 	Key   string `json:"key"`
 	Value string `json:"value"`
 }
 
-type Assignees struct {
-	Id                int                 `json:"id"`
-	Username          string              `json:"username"`
-	Public_email      string              `json:"public_email"`
-	Name              string              `json:"name"`
-	State             string              `json:"state"`
-	Locked            bool                `json:"locked"`
-	Avatar_url        string              `json:"avatar_url"`
-	Avatar_path       string              `json:"avatar_path"`
-	Custom_attributes []Custom_attributes `json:"custom_attributes"`
-	Web_url           string              `json:"web_url"`
+type Gitlab_Assignees struct {
+	Id                int                        `json:"id"`
+	Username          string                     `json:"username"`
+	Public_email      string                     `json:"public_email"`
+	Name              string                     `json:"name"`
+	State             string                     `json:"state"`
+	Locked            bool                       `json:"locked"`
+	Avatar_url        string                     `json:"avatar_url"`
+	Avatar_path       string                     `json:"avatar_path"`
+	Custom_attributes []Gitlab_Custom_attributes `json:"custom_attributes"`
+	Web_url           string                     `json:"web_url"`
 }
 
-type Author struct {
-	Id                int                 `json:"id"`
-	Username          string              `json:"username"`
-	Public_email      string              `json:"public_email"`
-	Name              string              `json:"name"`
-	State             string              `json:"state"`
-	Locked            bool                `json:"locked"`
-	Avatar_url        string              `json:"avatar_url"`
-	Avatar_path       string              `json:"avatar_path"`
-	Custom_attributes []Custom_attributes `json:"custom_attributes"`
-	Web_url           string              `json:"web_url"`
+type Gitlab_Author struct {
+	Id                int                        `json:"id"`
+	Username          string                     `json:"username"`
+	Public_email      string                     `json:"public_email"`
+	Name              string                     `json:"name"`
+	State             string                     `json:"state"`
+	Locked            bool                       `json:"locked"`
+	Avatar_url        string                     `json:"avatar_url"`
+	Avatar_path       string                     `json:"avatar_path"`
+	Custom_attributes []Gitlab_Custom_attributes `json:"custom_attributes"`
+	Web_url           string                     `json:"web_url"`
 }
 
-type Closed_by struct {
-	Id                int                 `json:"id"`
-	Username          string              `json:"username"`
-	Public_email      string              `json:"public_email"`
-	Name              string              `json:"name"`
-	State             string              `json:"state"`
-	Locked            bool                `json:"locked"`
-	Avatar_url        string              `json:"avatar_url"`
-	Avatar_path       string              `json:"avatar_path"`
-	Custom_attributes []Custom_attributes `json:"custom_attributes"`
-	Web_url           string              `json:"web_url"`
+type Gitlab_Closed_by struct {
+	Id                int                        `json:"id"`
+	Username          string                     `json:"username"`
+	Public_email      string                     `json:"public_email"`
+	Name              string                     `json:"name"`
+	State             string                     `json:"state"`
+	Locked            bool                       `json:"locked"`
+	Avatar_url        string                     `json:"avatar_url"`
+	Avatar_path       string                     `json:"avatar_path"`
+	Custom_attributes []Gitlab_Custom_attributes `json:"custom_attributes"`
+	Web_url           string                     `json:"web_url"`
 }
 
-type Iteration struct {
+type Gitlab_Iteration struct {
 	Id          string `json:"id"`
 	Iid         string `json:"iid"`
 	Sequence    string `json:"sequence"`
@@ -106,31 +106,31 @@ type Iteration struct {
 }
 
 type Create_Gitlab_Issue_Response struct {
-	Id                   int       `json:"id"`
-	Iid                  int       `json:"iid"`
-	Project_id           int       `json:"project_id"`
-	Title                string    `json:"title"`
-	Description          string    `json:"description"`
-	State                string    `json:"state"`
-	Created_at           string    `json:"created_at"`
-	Updated_at           string    `json:"updated_at"`
-	Closed_at            string    `json:"closed_at"`
-	Closed_by            Closed_by `json:"closed_by"`
-	Labels               string    `json:"labels"`
-	Milestone            Milestone `json:"milestone"`
-	Assignees            Assignee  `json:"assignees"`
-	Author               Author    `json:"author"`
-	Type                 string    `json:"type"`
-	Assignee             Assignee  `json:"assignee"`
-	User_notes_count     string    `json:"user_notes_count"`
-	Merge_requests_count string    `json:"merge_requests_count"`
-	Upvotes              string    `json:"upvotes"`
-	Downvotes            string    `json:"downvotes"`
-	Due_date             string    `json:"due_date"`
-	Confidential         bool      `json:"confidential"`
-	Discussion_locked    bool      `json:"discussion_locked"`
-	Issue_type           string    `json:"issue_type"`
-	Web_url              string    `json:"web_url"`
+	Id                   int              `json:"id"`
+	Iid                  int              `json:"iid"`
+	Project_id           int              `json:"project_id"`
+	Title                string           `json:"title"`
+	Description          string           `json:"description"`
+	State                string           `json:"state"`
+	Created_at           string           `json:"created_at"`
+	Updated_at           string           `json:"updated_at"`
+	Closed_at            string           `json:"closed_at"`
+	Closed_by            Gitlab_Closed_by `json:"closed_by"`
+	Labels               string           `json:"labels"`
+	Milestone            Gitlab_Milestone `json:"milestone"`
+	Assignees            Gitlab_Assignees `json:"assignees"`
+	Author               Gitlab_Author    `json:"author"`
+	Type                 string           `json:"type"`
+	Assignee             Gitlab_Assignees `json:"assignee"`
+	User_notes_count     string           `json:"user_notes_count"`
+	Merge_requests_count string           `json:"merge_requests_count"`
+	Upvotes              string           `json:"upvotes"`
+	Downvotes            string           `json:"downvotes"`
+	Due_date             string           `json:"due_date"`
+	Confidential         bool             `json:"confidential"`
+	Discussion_locked    bool             `json:"discussion_locked"`
+	Issue_type           string           `json:"issue_type"`
+	Web_url              string           `json:"web_url"`
 	Time_stats           struct {
 		Time_estimate          int    `json:"time_estimate"`
 		Total_time_spent       int    `json:"total_time_spent"`
@@ -170,36 +170,36 @@ type Create_Gitlab_Issue_Response struct {
 		Human_readable_end_date  string `json:"human_readable_end_date"`
 		Human_readable_timestamp string `json:"human_readable_timestamp"`
 	} `json:"epic"`
-	Iteration     Iteration `json:"iteration"`
-	Health_status string    `json:"health_status"`
+	Iteration     Gitlab_Iteration `json:"iteration"`
+	Health_status string           `json:"health_status"`
 }
 
 type Get_Gitlab_Issue_Response struct {
-	Id                   int       `json:"id"`
-	Iid                  int       `json:"iid"`
-	Project_id           int       `json:"project_id"`
-	Title                string    `json:"title"`
-	Description          string    `json:"description"`
-	State                string    `json:"state"`
-	Created_at           string    `json:"created_at"`
-	Updated_at           string    `json:"updated_at"`
-	Closed_at            string    `json:"closed_at"`
-	Closed_by            Closed_by `json:"closed_by"`
-	Labels               string    `json:"labels"`
-	Milestone            Milestone `json:"milestone"`
-	Assignees            Assignees `json:"assignees"`
-	Author               Author    `json:"author"`
-	Type                 string    `json:"type"`
-	Assignee             Assignee  `json:"assignee"`
-	User_notes_count     string    `json:"user_notes_count"`
-	Merge_requests_count string    `json:"merge_requests_count"`
-	Upvotes              string    `json:"upvotes"`
-	Downvotes            string    `json:"downvotes"`
-	Due_date             string    `json:"due_date"`
-	Confidential         bool      `json:"confidential"`
-	Discussion_locked    bool      `json:"discussion_locked"`
-	Issue_type           string    `json:"issue_type"`
-	Web_url              string    `json:"web_url"`
+	Id                   int              `json:"id"`
+	Iid                  int              `json:"iid"`
+	Project_id           int              `json:"project_id"`
+	Title                string           `json:"title"`
+	Description          string           `json:"description"`
+	State                string           `json:"state"`
+	Created_at           string           `json:"created_at"`
+	Updated_at           string           `json:"updated_at"`
+	Closed_at            string           `json:"closed_at"`
+	Closed_by            Gitlab_Closed_by `json:"closed_by"`
+	Labels               string           `json:"labels"`
+	Milestone            Gitlab_Milestone `json:"milestone"`
+	Assignees            Gitlab_Assignees `json:"assignees"`
+	Author               Gitlab_Author    `json:"author"`
+	Type                 string           `json:"type"`
+	Assignee             Gitlab_Assignees `json:"assignee"`
+	User_notes_count     string           `json:"user_notes_count"`
+	Merge_requests_count string           `json:"merge_requests_count"`
+	Upvotes              string           `json:"upvotes"`
+	Downvotes            string           `json:"downvotes"`
+	Due_date             string           `json:"due_date"`
+	Confidential         bool             `json:"confidential"`
+	Discussion_locked    bool             `json:"discussion_locked"`
+	Issue_type           string           `json:"issue_type"`
+	Web_url              string           `json:"web_url"`
 	Time_stats           struct {
 		Time_estimate          int    `json:"time_estimate"`
 		Total_time_spent       int    `json:"total_time_spent"`
@@ -239,8 +239,8 @@ type Get_Gitlab_Issue_Response struct {
 		Human_readable_end_date  string `json:"human_readable_end_date"`
 		Human_readable_timestamp string `json:"human_readable_timestamp"`
 	} `json:"epic"`
-	Iteration     Iteration `json:"iteration"`
-	Health_status string    `json:"health_status"`
+	Iteration     Gitlab_Iteration `json:"iteration"`
+	Health_status string           `json:"health_status"`
 }
 
 func (c Create_Gitlab_Issue) Create(title, description string) error {
